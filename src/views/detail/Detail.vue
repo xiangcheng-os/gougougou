6e969b73203da071afe981792e1faa0e9df475dd
mengxiangcheng2019 <572176737@qq.com>
2020-06-09 23:53:42 +0800

    太热了

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -6,11 +6,42 @@
       </div>
       <div slot="center">商品详情</div>
     </nav-bar>
+    <detail-swiper :message="detailSwiper"></detail-swiper>
+    <div class="price">￥{{detailData.goods_price}}</div>
+    <div class="title">
+      <div class="title-a">{{detailData.goods_price}}</div>
+      <div class="title-b">
+        <img src="~assets/img/common/collect.svg" alt="">
+        <span>收藏</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
+  name:'Detail',
+  data () {
+    return {
+      id:Number,
+      detailData:{}
+    }
+  },
+  components: {
+    NavBar,
+    detailSwiper,
+  },
+
+
+  computed: {
+    detailSwiper () {
+      return this.detailData.pics
+    }
+  },
+  methods: {
+
+  }
 
 }
 </script>
--- src/views/detail/childDetail/DetailSwiper.vue
+++ src/views/detail/childDetail/DetailSwiper.vue
@@ -2,18 +2,32 @@
   <div class="detail-swiper">
     <swiper class="swiper">
       <swiper-item v-for="(item, index) in message" :key="index">
-        <img src="item" alt="">
+        <img :src="item.pics_big" alt="">
       </swiper-item>
     </swiper>
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperItem } from 'components/common/swiper'
 export default {
+  name: 'DetailSwiper',
+  props: {
+    message: {
+      type: Array,
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
 
 }
 </script>
 
-<style>
+<style scoped>
+.swiper{
+  height: 234px;
+} 
 
 </style>
